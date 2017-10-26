40daf84208dbd6adc3bb5fc30f6713eaf0832c60
dmitrySamsonov <dmitry.samsonov.alexandrovich@gmail.com>
2017-10-26 13:24:27 +0300

    add table Street (directory), fill by default, selectMenu

---
+++ src/main/java/model/Address.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Address {
+    private int streetCode;
+    private int houseNumber;
+
+    public int getStreetCode() {
+        return streetCode;
+    }
+
+    public void setStreetCode(int streetCode) {
+        this.streetCode = streetCode;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
